b82103ae2d73ad54bc44bbc5fbb6dd16588cb6ac
Daniel Fox <danny.fox97@hotmail.com>
2025-12-13 15:48:44 +0000

    Removed active state to nav on scroll as not needed?

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -58,27 +58,3 @@ if (form && successMessage) {
   });
 }
 
-// Add active state to nav on scroll
-const sections = document.querySelectorAll('section[id]');
-const navLinks = document.querySelectorAll('.nav a');
-
-if (sections.length && navLinks.length) {
-  window.addEventListener('scroll', () => {
-    let current = '';
-
-    sections.forEach(section => {
-      const sectionTop = section.offsetTop;
-      const sectionHeight = section.clientHeight;
-      if (window.pageYOffset >= sectionTop - 100) {
-        current = section.getAttribute('id');
-      }
-    });
-
-    navLinks.forEach(link => {
-      link.classList.remove('active');
-      if (link.getAttribute('href') === `#${current}`) {
-        link.classList.add('active');
-      }
-    });
-  });
-}
